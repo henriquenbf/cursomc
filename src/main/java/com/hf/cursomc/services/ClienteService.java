55aad38667b4160ca7c42fc7ce8790c19f8c0c03
Henrique Fontana <henriquenbf@gmail.com>
2019-05-14 22:14:24 -0300

    Criação do endpoint de clientes

---
+++ src/main/java/com/hf/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.hf.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hf.cursomc.domain.Cliente;
+import com.hf.cursomc.exceptions.ObjectNotFoundException;
+import com.hf.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository ClienteRepo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> ClienteOtp = ClienteRepo.findById(id);
+        return ClienteOtp.orElseThrow(() -> new ObjectNotFoundException(
+                String.format("Objeto com ID = %s não encontrado. Tipo = %s", id, Cliente.class.getName())));
+    }
+
+}
