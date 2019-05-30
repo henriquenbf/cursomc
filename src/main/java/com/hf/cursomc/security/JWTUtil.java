bf4e6c3141e2e690865848277c0ea284de34e353
Henrique Fontana <henriquenbf@gmail.com>
2019-05-29 21:39:41 -0300

    Criação da validação de token para acesso aos endpoints protegidos

--- src/main/java/com/hf/cursomc/security/JWTUtil.java
+++ src/main/java/com/hf/cursomc/security/JWTUtil.java
@@ -1,10 +1,12 @@
 package com.hf.cursomc.security;
 
 import java.util.Date;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -21,6 +23,39 @@ public class JWTUtil {
         String jwtToken = Jwts.builder().setSubject(email).setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret.getBytes()).compact();
         return jwtToken;
+    }
+
+    public boolean tokenValido(String jwtToken) {
+        Claims claims = getClaims(jwtToken);
+
+        if (Objects.nonNull(claims)) {
+            String username = claims.getSubject();
+            Date expirationDate = claims.getExpiration();
+            Date now = new Date(System.currentTimeMillis());
+
+            if (Objects.nonNull(username) && Objects.nonNull(expirationDate) && now.before(expirationDate)) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    private Claims getClaims(String jwtToken) {
+        try {
+            return Jwts.parser().setSigningKey(jwtSecret.getBytes()).parseClaimsJws(jwtToken).getBody();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public String getUsername(String jwtToken) {
+        Claims claims = getClaims(jwtToken);
+        if (Objects.nonNull(claims)) {
+            return claims.getSubject();
+        }
+        return null;
     }
 
 }
